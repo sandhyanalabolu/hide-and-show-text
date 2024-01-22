5c6e6686f14c451cf8991716d7a97e141a3ca39a
sandhyanalabolu <157108144+sandhyanalabolu@users.noreply.github.com>
2024-01-22 20:59:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var div = document.getElementById("d2");
+var display = 0;
+
+function hideshow() {
+  if (display == 1) {
+    div.style.display = "block";
+    display = 0;
+  } else {
+    div.style.display = "none";
+    display = 1;
+  }
+}
